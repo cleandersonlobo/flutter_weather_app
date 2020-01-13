4763688f9298a2e2084e634db42ebcb077f91a2f
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-01-13 20:34:54 -0300

    Theme Colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static Color primaryColor = Color(0xff2482C7);
+  static Color secondaryColor = Color(0xff0848A4);
+  static Color blueAccent = Color(0xff204CFE);
+  static Color blueLight = Color(0xff27BDFF);
+  static Color red = Color(0xffFF3B3B);
+  static Color yellow = Color(0xffFFD60A);
+  static Color orange = Color(0xffFF9751);
+  static Color purple = Color(0xffFF1AFF);
+}
